78b724ad5d60ee8dbcc766cb73a021b3704716e7
Rafal Rudzinski <rrudzin1@sundevils.asu.edu>
2017-02-09 19:10:04 -0800

    Added Comic Book Detail view

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the combic books controller");
+            return View();
         }
     }
 }
